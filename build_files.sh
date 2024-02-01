bda23592a2b18806e8eff26688d0e59e39031bfe
uniquebiwas <uniquebiwas@gmail.com>
2024-02-01 19:18:04 +0545

    vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,10 @@
 # Build the project
 echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+# python3.9 -m pip install -r requirements.txt
 
 echo "Make Migration..."
 python3.9 manage.py makemigrations 
 python3.9 manage.py migrate 
 
 # echo "Collect Static..."
-# python3.9 manage.py collectstatic 
+python3.9 manage.py collectstatic 
