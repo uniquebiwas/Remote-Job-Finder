7945775fac05b56807ec8d828eeadd79a97ae184
uniquebiwas <uniquebiwas@gmail.com>
2024-02-01 23:37:32 +0545

    vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -8,3 +8,7 @@ python3.9 manage.py migrate
 
 # echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput
+
+
+echo "Collecting Media Files..."
+python3.9 manage.py collectstatic --noinput --ignore=*.js,*.css,*.scss,*.html,*.txt
